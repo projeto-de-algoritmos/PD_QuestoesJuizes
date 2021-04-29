177d4b9c2b7f4171564708fe63862efad0419769
Sergio de Almeida Cipriano Junior <sergiosacj@protonmail.com>
2021-04-29 14:18:55 -0300

    Explain solution 1034.cpp
    Co-authored-by: AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>

--- judges/URI/URI_1034/solution.cpp
+++ judges/URI/URI_1034/solution.cpp
@@ -12,26 +12,45 @@ using namespace std;
  
 int n, m;
 int ans[1000002];
+// n -> numero de instancias
+// m -> numero de tipos de blocos
+// ans -> vetor utilizado na memoizacao
  
+// OBJETIVO
+// Para cada instancia, imprima o numero minimo de blocos
+// necessarios para obter um bloco de comprimento m.
 int main(){
-	
-	scanf("%d", &n);
-
-	for(int i = 0; i < n; ++i)
-	{
-		int value;
-		scanf("%d %d", &m,&value);
-		int values[m];
-		for(int j = 0; j<m; ++j)
-			scanf("%d", &values[j]);
-
-		memset(ans, 999999, sizeof(ans));
-		ans[0] = 0;
-		for(int j = 0; j<m; ++j)
-			for(int k = values[j]; k<=value; k++)
-				ans[k] = min(ans[k], ans[k-values[j]]+1);
-		printf("%d\n", ans[value]);
-	}
-
-	return 0;
+    // lendo o numero de instancias
+    scanf("%d", &n);
+    for(int i = 0; i < n; ++i) {
+        int value;
+        // value -> comprimento desejado pelo artista
+        scanf("%d %d", &m, &value);
+        // inicializando um vetor com todos os comprimentos dos tipos
+        int values[m];
+        // lendo os comprimentos de cada tipo
+        for(int j = 0; j<m; ++j)
+            scanf("%d", &values[j]);
+        // definindo comprimento "infinito" na memoizacao
+        memset(ans, 999999, sizeof(ans));
+        // o numero minimo de blocos para obter um comprimento 0 é 0
+        ans[0] = 0;
+        // percorrendo todos os tipos de bloco
+        for(int j = 0; j<m; ++j)
+            // para cada tipo j, percorrer do valor k do comprimento
+            // de tipo j ate atingir um valor k desejado
+            for(int k = values[j]; k<=value; k++)
+                // para calcular o comprimento minimo na posicao k,
+                // eh preciso comparar o comprimento na posicao k com
+                // o comprimento na posicao k sem o acrescimo do tipo j.
+                // comprimento k -> ans[k]
+                // comprimento k - tipo j -> ans[k-values[j]]
+                // A ideia do +1 eh de acrescentar o tipo j nessa comparacao
+                ans[k] = min(ans[k], ans[k-values[j]]+1);
+        // Ao final de todas as iteracoes basta acessar
+        // a posicao com o comprimento desejado (todas
+        // as posicoes percorridas possuem valor minimo
+        printf("%d\n", ans[value]);
+    }
+    return 0;
 }
