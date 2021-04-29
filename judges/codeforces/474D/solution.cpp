a76c53cf22d5b3f63a504b5f8ae43a09752c006e
Sergio de Almeida Cipriano Junior <sergiosacj@protonmail.com>
2021-04-29 18:59:50 -0300

    Explain solution 474D.cpp
    Co-authored-by: AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>

--- judges/codeforces/474D/solution.cpp
+++ judges/codeforces/474D/solution.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
- 
+
+// Conjunto de definicoes muito uteis para ganhar tempo nas maratonas
 #define ll long long
 #define ii pair<int, int>
 #define vi vector<int>
@@ -15,6 +16,10 @@
 #define oo 1e9
 #define max 1e9+7
  
+// Esse daqui eh um truque bem bacana e util. Ajuda a debugar o codigo
+// rapidamente, evitando perder tempo apagando os prints adicionados, ja
+// que o juiz online vai ignorar os prints gracas ao ifndef. Outra coisa
+// bacana eh a cor que ajuda a diferenciar dos prints padroes.
 #ifndef ONLINE_JUDGE
 #define W(x) cerr << "\033[31m" << #x << " = " << x << "\033[0m" << "\n";
 #else
@@ -23,29 +28,56 @@
  
 using namespace std;
  
+//  t -> numero de casos de teste
+//  k -> tamanho dos grupos
+//  a, b -> flores
+//  prime -> primo definido na propria questao
 int t, k, a, b, prime = max;
+// fib -> vetor de memoizacao (o nome dele eh fib porque a programacao dinamica
+// parece muito com uma formula de finonacci)
+// ans -> vetor que vai armazenar as repostas do problema
 ll fib[100100], ans[100100];
  
-int main(){
+int main()
+{
 	scanf("%d %d", &t, &k);
- 
-	for (int i = 0; i < 100100; ++i){
+    // t e k sao sempre menores que 10^5 (esses 100 a mais sao so pra evitar dor
+    // de cabeca caso o autor do problema tenha deixado algum erro passar nos
+    // casos de teste) e, por conta disso, podemos precomputar todas as
+    // respostas da questao previamente.
+	for (int i = 0; i < 100100; ++i)
+    {
+        // As folhas brancas so podem ser comidas em grupos de k elementos,
+        // sendo assim, so existe uma combinacao possivel para valores menores
+        // do que k.
 		if(i < k) fib[i] = 1;
-		else{
-			fib[i] = (fib[i-1]+fib[i-k])%prime;
-		}
+        // Aqui esta o pulo do gato, fib[i] nada mais eh do que o fib
+        // anterior acrescido de folhas vermelhas mais o fib[i-k] que
+        // tem o acrescimo das folhas com k combinacoes.
+        // Para ficar mais claro:
+        // fib[i-1] -> esse aparece novamente com a unica diferenca de que foram
+        // somados novas folhas vermelhas (R) a combinacao
+        // fib[i-k] -> mesma ideia do de cima com a diferenca que ao inves de
+        // somar 1 folha vermelha em cada, eh somado k folhas brancas em cada
+        // grupo.
+		else fib[i] = (fib[i-1]+fib[i-k])%prime;
 	}
- 
-	for (int i = 1; i < 100100; ++i){
+    // O loop anterior ja contem toda a logica essencial para resolver o
+    // problema, esse loop a seguir eh responsavel por fazer a soma de
+    // todas as solucoes para que, quando for a hora de calcular as
+    // combinacoes de um intervalo, seja possivel calcular em O(1).
+	for (int i = 1; i < 100100; ++i)
+    {
 		if(i == 1) ans[i] = fib[i];
 		else ans[i] = (ans[i-1] + fib[i])%prime;
 	}
- 
-	while(t--){
+	while(t--)
+    {
+        // Lendo o intervalo
 		scanf("%d %d", &a, &b);
- 
+        // Calculando as quantidades de combinacoes dentro do intervalo em O(1).
+        // O motivo de usar ans[a-1] eh porque ans[a] esta incluso no intervalo
 		printf("%lld\n", (ans[b]+prime - ans[a-1])%prime);
 	}
-	
 	return 0;
 }
