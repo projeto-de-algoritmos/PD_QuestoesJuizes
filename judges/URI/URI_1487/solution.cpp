e0a9e39bab5c74e6aa83655a098f841d6c2fbbaa
Sergio de Almeida Cipriano Junior <sergiosacj@protonmail.com>
2021-04-29 15:16:23 -0300

    Explain solution 1487.cpp
    Co-authored-by: AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>

--- judges/URI/URI_1487/solution.cpp
+++ judges/URI/URI_1487/solution.cpp
@@ -13,8 +13,8 @@ int V[MAX_ITENS];
 // matriz de memorização
 int memo[MAX_W][MAX_V];
 
-int knapsack(int i, int w) {
-
+int knapsack(int i, int w)
+{
     // Caso o item não exista ou não mais caiba na mochila
     // nenhum valor será agregado
     if (i < 0 || w <= 0) return 0;
@@ -29,27 +29,37 @@ int knapsack(int i, int w) {
     // Caso contrário o valor ótimo será o maior valor entre a decisão
     // de não carregar o item e carregar o item
     return memo[i][w] = max(max(knapsack(i, w - W[i]) + V[i], knapsack(i - 1, w - W[i]) + V[i]), knapsack(i - 1, w));
-                            
 }
 
-int main() {
-    // Inicialização da matriz de memoização com -1
-    
-    int n,t, i = 0, num = 0;
+int main()
+{
+    // n -> numero de atracoes que colegas gostariam de brincar
+    // t -> tempo disponivel
+    // num -> instancia
+    // i -> variavel auxiliar para loops
+    int n, t, i = 0, num = 0;
+    // Enquanto tiver atracoes para brincar...
     while(cin >> n >> t, n){
+        // inicializando memoizacao com "infinito"
         memset(memo, -1, sizeof memo);
-        memset(W, 0, sizeof W);
+        // zerando pesos e valores
         memset(V, 0, sizeof V);
+        memset(W, 0, sizeof W);
+        // incrementar instancia
         num++;
+        // ler W[i] e V[i]
+        // W[i] -> duracao do brinquedo (peso)
+        // V[i] -> pontuacao atribuida ao brinquedo (valor)
         i = 0;
         while(i<n){
             cin >> W[i] >> V[i];
             i++;
         }   
-        cout << "Instancia " << num << "\n"; 
-        cout << knapsack(n-1,t) << endl;
-        cout << endl;
+        // instancia atual
+        cout << "Instancia " << num << endl; 
+        // colecao (pode haver repeticoes) de
+        // atracoes que da a maior pontuacao
+        cout << knapsack(n-1,t) << endl << endl;
     }    
-    
     return 0;    
 }
